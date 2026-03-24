56807af0b0875516f5e6e4e79138c7f76b0fc220
nietogs21 <susan.nieto01@usa.edu.co>
2026-03-24 12:35:07 -0500

    disparo ya configurado

--- My project (1)/Assets/_scripts/GunController.cs
+++ My project (1)/Assets/_scripts/GunController.cs
@@ -1,4 +1,4 @@
-
+using System.Collections;
 using UnityEngine;
 
 public class GunController : MonoBehaviour
@@ -44,7 +44,7 @@ public class GunController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Fire();
     }
 
     // Update is called once per frame
@@ -53,4 +53,62 @@ public class GunController : MonoBehaviour
         gunTrigger.size = new Vector3(weapon.horizontalRange, weapon.verticalRange, weapon.range);
         gunTrigger.center = new Vector3(0, (0.5f * weapon.verticalRange - 1f), weapon.range * 0.5f);
     }
+
+    private void OnTriggerEnter(Collider other) //en other se almacena el objeto que entra
+    {
+        Enemy enemy = other.GetComponent<Enemy>();
+        if (enemy)
+        {
+            enemy.GetComponent<Renderer>().material = detectecMaterial;
+            EnemyManager.instance.AddEnemy(enemy);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Enemy enemy = other.GetComponent<Enemy>();
+        if (enemy)
+        {
+            enemy.GetComponent<Renderer>().material = initialMaterial;
+            EnemyManager.instance.RemoveEnemy(enemy);
+        }
+    }
+
+    public void Fire()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)&& canShoot == true)
+        {
+            audioSource.PlayOneShot(weapon.sound);
+
+            foreach(Enemy enemy in EnemyManager.instance.enemiesInRange)
+            {
+                var dir = (enemy.transform.position - transform.position).normalized; //esto es para la direccion de los elementos
+
+                RaycastHit hit;
+
+                if(Physics.Raycast(transform.position, dir, out hit, weapon.range * 1.5f, rayscastLayer)) //se pone el 1,5 para que la bala agarre mas fuerza y si le de al objetivo
+                {
+                    if(hit.transform == enemy.transform)
+                    {
+                        Quaternion rot = Quaternion.LookRotation(-hit.normal);
+                        enemy.Damage(weapon.damage, rot);
+                    }
+
+                    Debug.DrawRay(transform.position, dir * weapon.range, Color.green, 1f );
+                }                                                                                           
+
+            }
+            StartCoroutine(CanFire(weapon.fireRate));
+
+        }
+    }
+    
+
+    IEnumerator CanFire(float time)
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(time);
+        canShoot = true;
+    }
 }
+   
